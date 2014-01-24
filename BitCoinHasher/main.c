803d5f7a871e3c6940c22e2d4d2691699311d653
Rob Marano <rob@thehackerati.com>
2014-01-23 22:10:42 -0500

    Initial commit from local git

---
+++ BitCoinHasher/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  BitCoinHasher
+//
+//  Created by Rob Marano on 1/23/14.
+//  Copyright (c) 2014 The Hackerati, Inc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
